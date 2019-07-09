d53742760ee252998417bdb115dbdce077057f4c
spartan1991 <vc_spartan@ukr.net>
2019-07-09 22:00:05 +0300

    1st. commit

---
+++ src/main/java/com/vcprojects/controllers/SimpleController.java
@@ -0,0 +1,14 @@
+package com.vcprojects.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SimpleController {
+
+    @GetMapping({"/"})
+    public String showSimplePage() {
+
+        return "index";
+    }
+}
---
+++ target/cleanSpringMVC-1.0-SNAPSHOT/WEB-INF/templates/index.ftl
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>main page</title>
+	</head>
+	<body>
+		<h1>Main page!</h1>
+	</body>
+</html>
